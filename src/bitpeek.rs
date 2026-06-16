662422c8bfe3be6b3f54d01ae613a6c52f171333
Stefan Kerkmann <s.kerkmann@pengutronix.de>
2026-06-16 15:51:06 +0200

    extension: search for lsp in path or use path from settings

--- src/bitpeek.rs
+++ src/bitpeek.rs
@@ -1,50 +1,56 @@
-#[cfg(target_arch = "wasm32")]
-mod wasm_extension {
-    use zed_extension_api::{self as zed};
+use zed_extension_api::{self as zed, LanguageServerId, Result, settings::LspSettings};
 
-    const SERVER_BINARY: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/target/release/bitpeek-ls");
+struct BitPeekExtension;
 
-    struct BitPeekExtension;
-
-    impl zed::Extension for BitPeekExtension {
-        fn new() -> Self {
-            Self {}
-        }
-
-        fn language_server_command(
-            &mut self,
-            _language_server_id: &zed::LanguageServerId,
-            _worktree: &zed::Worktree,
-        ) -> zed::Result<zed::Command> {
-            Ok(zed::Command {
-                command: SERVER_BINARY.into(),
-                args: vec![],
-                env: Default::default(),
-            })
-        }
-
-        fn language_server_initialization_options(
-            &mut self,
-            language_server_id: &zed::LanguageServerId,
-            worktree: &zed::Worktree,
-        ) -> zed::Result<Option<zed::serde_json::Value>> {
-            Ok(
-                zed::settings::LspSettings::for_worktree(language_server_id.as_ref(), worktree)?
-                    .initialization_options,
-            )
-        }
-
-        fn language_server_workspace_configuration(
-            &mut self,
-            language_server_id: &zed::LanguageServerId,
-            worktree: &zed::Worktree,
-        ) -> zed::Result<Option<zed::serde_json::Value>> {
-            Ok(
-                zed::settings::LspSettings::for_worktree(language_server_id.as_ref(), worktree)?
-                    .settings,
-            )
-        }
+impl zed::Extension for BitPeekExtension {
+    fn new() -> Self {
+        Self {}
     }
 
-    zed::register_extension!(BitPeekExtension);
+    fn language_server_command(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        let mut command = worktree.which(language_server_id.as_ref());
+
+        if let Ok(lsp_settings) = LspSettings::for_worktree(language_server_id.as_ref(), worktree) {
+            if let Some(command_settings) = lsp_settings.binary {
+                if command_settings.path.is_some() {
+                    command = command_settings.path;
+                }
+            }
+        };
+
+        Ok(zed::Command {
+            args: vec![],
+            command: command
+                .ok_or_else(|| format!("{} not found!", language_server_id.as_ref()))?,
+            env: Default::default(),
+        })
+    }
+
+    fn language_server_initialization_options(
+        &mut self,
+        language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> zed::Result<Option<zed::serde_json::Value>> {
+        Ok(
+            zed::settings::LspSettings::for_worktree(language_server_id.as_ref(), worktree)?
+                .initialization_options,
+        )
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> zed::Result<Option<zed::serde_json::Value>> {
+        Ok(
+            zed::settings::LspSettings::for_worktree(language_server_id.as_ref(), worktree)?
+                .settings,
+        )
+    }
 }
+
+zed::register_extension!(BitPeekExtension);
